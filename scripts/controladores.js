5bdb22cfc5a22ae1f7673c2b616aa4469f1325d5
JrClemente <165841796+jrclemente@users.noreply.github.com>
2024-07-21 14:51:15 -0300

    Update controladores.js

--- scripts/controladores.js
+++ scripts/controladores.js
@@ -1,6 +1,5 @@
 // import { AnoAtual } from './util.js';
-let pathName = location.pathname;
- console.log(pathName)   
+let pathName = location.pathname; 
 
 // Controle jquery para chamar a página LGPD
 $(document).ready(function () {
